932e544b025c45c9125bcaf425f1a1e304a152ea
rootofbug <164196905+rootofbug@users.noreply.github.com>
2024-10-14 17:06:15 +0600

    Update script.js

--- script.js
+++ script.js
@@ -28,8 +28,8 @@ document.addEventListener('DOMContentLoaded', function () {
         setTimeout(() => {
             if (type === 'normal') {
                 videoData = [
-                    { title: 'Normal Video 1', src: 'normal/sex 1.mp4' },
-                    { title: 'Normal Video 2', src: 'normal/sex 2.mp4' }
+                    { title: 'Sex 1', src: 'normal/sex 1.mp4' },
+                    { title: 'Sex 2', src: 'normal/sex 2.mp4' }
                 ];
             } else if (type === 'gay') {
                 videoData = [
