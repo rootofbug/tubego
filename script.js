d96e37577dffb326118edb1baa3e6aa65f9e9ec5
rootofbug <164196905+rootofbug@users.noreply.github.com>
2024-10-14 18:10:26 +0600

    Update script.js

--- script.js
+++ script.js
@@ -29,7 +29,13 @@ document.addEventListener('DOMContentLoaded', function () {
             if (type === 'normal') {
                 videoData = [
                     { title: 'Sex 1', src: 'normal/sex-1.mp4' },
-                    { title: 'Sex 2', src: 'normal/sex-2.mp4' }
+                    { title: 'Sex 2', src: 'normal/sex-2.mp4' },
+                    { title: 'Sex 2', src: 'normal/sex-3.mp4' },
+                    { title: 'Sex 2', src: 'normal/sex-4.mp4' },
+                    { title: 'Sex 2', src: 'normal/sex-5.mp4' },
+                    { title: 'Sex 2', src: 'normal/sex-6.mp4' },
+                    { title: 'Sex 2', src: 'normal/sex-7.mp4' },
+                    { title: 'Sex 2', src: 'normal/sex-8.mp4' }
                 ];
             } else if (type === 'gay') {
                 videoData = [
